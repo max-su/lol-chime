c140a1ccbbc1b64133afd3c438a867af0a6eaf15
Max Su <max.su7@gmail.com>
2016-05-29 10:45:27 -0400

    Added localhost:8080 stuff, refactored old code made more clean, reduced callBackHell Parameters by making it an array of functions

--- js/serverScript.js
+++ js/serverScript.js
@@ -2,12 +2,28 @@ require("dotenv").config(); //keep api key in .env
 
 var request = require("request");
 var sleep = require("sleep");
+var http = require("http");
+
 /*USAGE
  *typeOfCall = summonerLookup OR gameLookup
  *region = na, eu, etc.
  *id = summonerName OR summonerID
  */
-function getURL(typeOfCall, region, id) { 
+
+var PORT = 8080;
+var handleRequest = function (request, response){
+	response.end("It works. Path hit: " + request.url);
+};
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+		console.log("Server listening on: http://localhost:%s", PORT);
+	}
+);
+
+//____API STUFF FOLLOWS
+var getUrl = function (typeOfCall, region, id) { 
 	result = "https://" + region + ".api.pvp.net/";
 	// at this point we should have something like https://na.api.pvp.net/
 	// do /version/whateverAPIQuery
@@ -16,20 +32,28 @@ function getURL(typeOfCall, region, id) {
 			result += "api/lol/" + region + "/v1.4/summoner/by-name/";
 			break;
 		case "gameLookUp":
-			result += "observer-mode/rest/consumer/getSpectatorGameInfo/" + getregionID(region)+ "/";
+			result += "observer-mode/rest/consumer/getSpectatorGameInfo/" + getRegionID(region)+ "/";
 			break;
 	}
 	result += id + "?api_key=" + process.env.APIKEY;
 	return result;
-}
-
+};
+/*
 var URL = getURL("summonerLookUp","NA","Quantum Bogosort");
 console.log(URL + "\n");
 var URL2 = getURL("gameLookUp","NA","20198954");
 console.log(URL2 + "\n");
+*/
+var cleanSummonerName = function (summonerName) {
+	var ignTrim = summonerName.replace(" ","");
+	ignTrim = ignTrim.toLowerCase();
+	ignTrim = ignTrim.trim();
+	return ignTrim;
+};
 
-function getregionID(region) {
-	switch(region){
+
+var getRegionID = function (region) {
+	switch(region) {
 		case "BR":
 			return "BR1";
 		case "EUNE":
@@ -55,45 +79,120 @@ function getregionID(region) {
 		default:
 			return "Error!";
 	}
-}
+};
 
-
-function getSummoner(summonerName, region, callBack) { //we need to do lookup with summoner ID(an int), not a string
+/*______-YOU ARE NOW ENTERING CALLBACK HELL
+//PREPARE URSELF
+            _.'          .
+           |P`            ?\
+          ."h              "B
+          (""h             "P
+          ?""",          .""P
+          {"``""oo____oo""""P
+           '""888888888888,;
+            `?88P^\,?88^\,Y
+              88?\__d88\_/'
+              `8o8888/\88P
+               ,?oo88oo8P
+     ___  __===~88~\\\\\|~====__ __       ___
+ .-==ooo~odoooob ?8/////'oooood88888ooo?P88888?ooo?888PooooOoooooooo=~--
+d,d8888obo8,oo8b,``~~~,o?8oo,8888**8P88?8`oo,o888?898o8P888P~8b
+8o88888oP?.,,ooood8b,.oo.,oo88?o8888P^^^?8888P^^^^==+=~~~?8bo?P
+?8.=~=.8do.,oo88888ooo,o,oo88888o,;                      `6
+.?*o88ob`8.,o88888888oo,o,o88888o,'                       `\
+|*o8888o`8,oo88888888oo,o,o?8888o'
+`?o8888P`88,,oo88888oo,,.,oo88oo;
+.8`o888'**888oo,,,,oood88oo,,d8'
+doo,,oP  ?888o,,o8o,o8o,,,o8oo'
+8od8bo'  `?88o,,o8o,o88o,.,o,o'
+8o888o    `88o,,o8o,o8o,.,o,o'
+?o888P     ===========<666>==
+`?88P     ********************
+ ?ooPb   ',odoo,************,b
+d{{{{b\,(,oo88ooo,,********,oob
+OOOOO \\,oo8888ooo,,******,o888,
+(\\\\ ||,o8888888oo,,*****,o88o)
+  \\\\/`,o8888888oo',*****,888o|
+        `oo8888888o',*****,888o|
+        `,o888888oo,,*****,o8oo'
+        `,o88888oo,, *****,o8oP
+        `?,o888oP,,  *****,ooP
+         `8-==-P,    |****,,-='
+          ?o88oP     |*'d*,o88
+         .dboodb.     *;8*,o88
+         ,od88boo     *,o*,o88'
+         do8888ob     *`o*,o8P
+         ?o8888oP     |`,|,oo'
+         `o8888o'     `?o8oP
+          `o88o'      ,o88o'
+           ?ooP       `?88o'
+          dooo'b      'o88oo,.
+         '?o===o   ~-=8oo888888oo.__
+         8888888     `~~~' `==~~~=-,' 
+         ?8P?88P 
+          V''V'     
+		  ok ur brain will turn into putty in 2 lines.
+FIRST FUNCTION IN CALLBACK HELL*/
+var getSummonerID = function (summonerName, region, callBackFunctions) { //we need to do lookup with summoner ID(an int), not a string
 	request({
-		url: getURL("summonerLookUp", region, summonerName),
+		url: getUrl("summonerLookUp", region, summonerName),
 		json: true
 		}, 
 		function (error, response, body) {
-			if(!error && response.statusCode === 200){
-				callBack(body, error, summonerName, true); //because this is ASynchronous i/o, 
-				
-			} else {
-				callBack(body, error, summonerName, false);
+			if(!error && response.statusCode === 200) {
+				callBackFunctions[0](body, error, summonerName, true, region, callBackFunctions); //because this is ASynchronous i/o, 			
+				//callBackSummonerID
+			}else {
+				callBackFunctions[0](body, error, summonerName, false, region, callBackFunctions);
+				//callBackSummonerID
 			}
-		}
+		}		
 	);
-	//return body;
-}
+};
 
-function cleanSummonerName(summonerName) {
-	var ignTrim = summonerName.replace(" ","");
-	ignTrim = ignTrim.toLowerCase();
-	ignTrim = ignTrim.trim();
-	return ignTrim;
-}
-
-var callBackSummonerID = function(body, error, summonerName, noError) { //summonerJSON must be declared!
+//callBackFunctions[0]
+var callBackSummonerID = function (body, error, summonerName, noError, region, callBackFunctions) { //summonerJSON must be declared!
 	if(noError === true) {
 		summoner = body;
-		console.log(summoner);
-		ign = cleanSummonerName(summonerName);
-		console.log(summoner[ign].id);
+		console.log(summoner);	
+		id = summoner[cleanSummonerName(summonerName)].id;
+		callBackFunctions[1](id, region, callBackFunctions, false); 
+		//checkSummonerInGame
 	}else if(noError === false) {
 		summonerError = error;
 		console.log("Error: " + Error);
+		callBackFunctions[2]();
 	}
 };
 
-var summonerError = {};
-summoner = getSummoner("Quantum Bogosort", "NA", callBackSummonerID);
+//callBackFunctions[1]
+var checkSummonerInGame = function (id, region, callBackFunctions, firstTimeQuery){
+	console.log(id);
+	request({
+		url: getUrl("gameLookUp", region, id),
+		json: true
+		},
+		function (error, response, body) {
+			if(!error && response.statusCode === 200) { //GAME FOUND
+				
+				console.log(body);
+				
+			}else if(!error && response.statusCode === 404) { //NO GAME FOUND
+				console.log(body);
+			}else {
+				console.log(error);	
+			}
+		}
+	);
+};
 
+//callBackFunctions[2]
+var nameNotFoundDoStuff = function (){
+	
+
+};
+
+
+
+var callBackFunctionList = [callBackSummonerID, checkSummonerInGame, nameNotFoundDoStuff];
+summonerID = getSummonerID("Quantum Bogosort", "NA", callBackFunctionList);
