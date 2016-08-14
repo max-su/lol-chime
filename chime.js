547650c518e9d56b769c625acf1e7e8be087566b
Max Su <max.su7@gmail.com>
2016-08-14 00:06:03 -0400

    fixed chime.js

--- chime.js
+++ chime.js
@@ -3,5 +3,5 @@ var argv = require('minimist')(process.argv.slice(2));
 var SummonerEmitter = require("./leagueLib/SummonerEmitter");
 var leagueLib = require("./leagueLib/leagueLib");
 
-summonerTest = new SummonerEmitter(argv[summoner], process.env.REGION);
+summonerTest = new SummonerEmitter(argv.summoner, process.env.REGION);
 leagueLib.initializeEvents(summonerTest);
